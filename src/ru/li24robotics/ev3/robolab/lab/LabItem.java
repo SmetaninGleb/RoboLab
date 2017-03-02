0d9e6b338e901a831e1eefca584b1e6e76e858cb
Gleb <Gleb>
2017-03-02 18:53:19 +0300

    start to create lab analysation

---
+++ src/ru/li24robotics/ev3/robolab/lab/LabItem.java
@@ -0,0 +1,9 @@
+package ru.li24robotics.ev3.robolab.lab;
+
+public class LabItem {
+	public boolean toRight;
+	public boolean toDown;
+	public boolean toLeft;
+	public boolean toUp;
+	public boolean isCubeHere;
+}
