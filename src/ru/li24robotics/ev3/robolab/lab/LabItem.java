13eb016579483fa745c7d666fbe92b854f4cfb30
Gleb <Gleb>
2017-03-06 07:59:25 +0300

    init

--- src/ru/li24robotics/ev3/robolab/lab/LabItem.java
+++
@@ -1,9 +0,0 @@
-package ru.li24robotics.ev3.robolab.lab;
-
-public class LabItem {
-	public boolean toRight;
-	public boolean toDown;
-	public boolean toLeft;
-	public boolean toUp;
-	public boolean isCubeHere;
-}
